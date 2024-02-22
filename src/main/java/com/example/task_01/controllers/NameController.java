063afcebd0aeb6e392038a3a936f0bd596de2f71
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-02-22 15:14:31 +0000

    created Greeting POJO

--- src/main/java/com/example/task_01/controllers/NameController.java
+++ src/main/java/com/example/task_01/controllers/NameController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class NameController {
 
     @GetMapping()
-    public String greeting() {
+    public String displayGreeting() {
         return "Good afternoon!";
     }
 
