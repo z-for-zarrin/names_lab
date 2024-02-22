3ccc63d30cfbe76c5531d175b582a832a1454d47
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-02-22 15:19:27 +0000

    refactored controller to use POJO instead of hardcoded string

--- src/main/java/com/example/task_01/controllers/NameController.java
+++ src/main/java/com/example/task_01/controllers/NameController.java
@@ -1,5 +1,6 @@
 package com.example.task_01.controllers;
 
+import com.example.task_01.models.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class NameController {
 
     @GetMapping()
-    public String displayGreeting() {
-        return "Good afternoon!";
+    public String displayGreeting(Greeting greeting) {
+        return String.format("Good %s, %s!", greeting.getTimeOfDay(), greeting.getName());
     }
 
 }
