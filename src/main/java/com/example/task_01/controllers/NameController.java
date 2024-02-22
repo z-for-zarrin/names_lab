f4bd79c26559c8fe299567ddff9dfb8fd902f174
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-02-22 15:08:46 +0000

    created controller to accept requests

---
+++ src/main/java/com/example/task_01/controllers/NameController.java
@@ -0,0 +1,10 @@
+package com.example.task_01.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/greeting")
+public class NameController {
+
+}
