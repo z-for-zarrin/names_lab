1d0fa9c44fbb6e2c4e280ae5d18ddd5b820ec185
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-02-22 16:14:24 +0000

    created Celebration DTO

---
+++ src/main/java/com/example/task_01/models/Celebration.java
@@ -0,0 +1,13 @@
+package com.example.task_01.models;
+
+public class Celebration {
+
+    private String message;
+
+    public Celebration() {}
+
+    public Celebration(String message) {
+        this.message = message;
+    }
+
+}
